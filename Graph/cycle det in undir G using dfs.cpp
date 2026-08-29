ba7f43ff996f877a52143adacd088ac30b0ca5d1
Dheeraj Kumar <dheeraj2525dk@gmail.com>
2026-08-29 12:59:25 +0530

    add cycle detect using dfs

---
+++ Graph/cycle det in undir G using dfs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    bool  dfs(int node, int parent, vector<int> &vis, vector<int> adj[]){
+        vis[node] = 1;
+        for(auto adjnode : adj[node]){
+            if(!vis[adjnode]){
+                if(dfs(adjnode,node,vis,adj)==true){
+                    return true; //there is a cycle
+                }
+            }
+            else if(adjnode != parent) return true;
+        }
+        return false;
+    }
+
+    bool Iscycle(int V, vector<vector<int>>& edges) {
+        vector<int> Adj[numCourses];
+        for(auto it : prerequisites){
+            Adj[it[0]].push_back(it[1]); 
+        }
+        vector<int>vis(V,0);
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+               if(dfs(1,-1,vis,adj)) return true;
+            }
+        }
+        return false;
+        
+
+    }
+};
